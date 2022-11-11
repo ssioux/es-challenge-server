502067b2a8c93c3cfda5b31fa82775951075d322
David Lazaro <dlazaro7@gmail.com>
2022-11-11 11:24:53 +0100

    starting rest Routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,6 +9,18 @@ router.get("/", (req, res, next) => {
 const authRoutes = require("./auth.routes")
 router.use("/auth", authRoutes)
 
+const gameRoutes = require("./game.routes")
+router.use("/game", gameRoutes)
+
+const teamRoutes = require("./team.routes")
+router.use("/team", teamRoutes)
+
+const tourneyRoutes = require("./tourney.routes")
+router.use("/tourney", tourneyRoutes)
+
+const profileRoutes = require("./profile.routes")
+router.use("/profile", profileRoutes)
+
 
 module.exports = router;
 
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -13,4 +13,5 @@ const router = require("express").Router();
 
 
 
+
 module.exports = router;
